840fdff1ae732874cde3ad7604a4af94393561b6
Saurav-Raj18 <sauravrajbittu95@gmail.com>
2024-12-19 13:30:04 +0530

    added route for admin panel

---
+++ backend/middlewares/multer.js
@@ -0,0 +1,20 @@
+/* Multer is a middleware for handling multipart/form-data, which is primarily used for uploading files in Node.js applications.to handle file uploads efficiently. */
+
+import multer from "multer";
+
+/* 
+multer.diskStorage():
+This specifies that files should be stored on the server's disk (local storage) instead of memory or cloud.
+*/
+
+//storage configuration
+const storage=multer.diskStorage({
+    filename:function(req,file,callback){
+        callback(null,file.originalname)
+    }
+})
+
+//create multer upload instance
+const upload=multer({storage})
+
+export default upload
---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/mongoDB.js';
+import connectCloudinary from './config/cloudinary.js';
+import adminRouter from './routes/adminRoute.js';
+
+// App config
+const app = express();
+const port = process.env.PORT || 5000;
+connectDB();
+connectCloudinary();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// API endpoints
+app.use('/api/admin', adminRouter);
+
+app.get('/', (req, res) => {
+  res.send('API working fine');
+});
+
+app.listen(port, () => {
+  console.log('Server started listening on the port:', port);
+});
